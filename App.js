262093fa783901c491d98e7924e0ee991d9a07db
Allen Hai <hai@growinfinitely.com>
2018-02-12 12:22:50 -0600

    full list of 99 names with basic styles, need to fix top offset

---
+++ App.js
@@ -0,0 +1,353 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <View style={styles.parentContainer}>
+      <View>
+        <Text style={styles.headerText}>
+          الله
+        </Text>
+      </View>
+      <View style={styles.namesContainer}>
+        <ScrollView>
+          <Text style={styles.text}>
+            Allah (الله)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Rahman (الرحمن)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Rahim (الرحيم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Malik (الملك)
+          </Text>
+          <Text style={styles.text}>
+            Al-Quddus (القدوس)
+          </Text>
+          <Text style={styles.text}>
+            As-Salam (السلام)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mu'min (المؤمن)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muhaymin (المهيمن)
+          </Text>
+          <Text style={styles.text}>
+            Al-Aziz (العزيز)
+          </Text>
+          <Text style={styles.text}>
+            Al-Jabbar (الجبار)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mutakabbir (المتكبر)
+          </Text>
+          <Text style={styles.text}>
+            Al-Khaliq (الخالق)
+          </Text>
+          <Text style={styles.text}>
+            Al-Bari' (البارئ)
+          </Text>
+          <Text style={styles.text}>
+            Al-Musawwir (المصور)
+          </Text>
+          <Text style={styles.text}>
+            Al-Ghaffar (الغفار)
+          </Text>
+          <Text style={styles.text}>
+            Al-Qahhar (القهار)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wahhab (الوهاب)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Razzaq (الرزاق)
+          </Text>
+          <Text style={styles.text}>
+            Al-Fattah (الفتاح)
+          </Text>
+          <Text style={styles.text}>
+            Al-`Alim (العليم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Qabid (القابض)
+          </Text>
+          <Text style={styles.text}>
+            Al-Basit (الباسط)
+          </Text>
+          <Text style={styles.text}>
+            Al-Khafid (الخافض)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Rafi (الرافع)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mu'izz (المعز)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mudhill (المذل)
+          </Text>
+          <Text style={styles.text}>
+            As-Sami (السميع)
+          </Text>
+          <Text style={styles.text}>
+            Al-Basir (البصير)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hakam (الحكم)
+          </Text>
+          <Text style={styles.text}>
+            Al-`Adl (العدل)
+          </Text>
+          <Text style={styles.text}>
+            Al-Latif (اللطيف)
+          </Text>
+          <Text style={styles.text}>
+            Al-Khabir (الخبير)
+          </Text>
+          <Text style={styles.text}>
+            Al-Halim (الحليم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Azim (العظيم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Ghafur (الغفور)
+          </Text>
+          <Text style={styles.text}>
+            Ash-Shakur (الشكور)
+          </Text>
+          <Text style={styles.text}>
+            Al-Ali (العلى)
+          </Text>
+          <Text style={styles.text}>
+            Al-Kabir (الكبير)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hafiz (الحفيظ)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muqit (المقيت)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hasib (الحسيب)
+          </Text>
+          <Text style={styles.text}>
+            Al-Jalil (الجليل)
+          </Text>
+          <Text style={styles.text}>
+            Al-Karim (الكريم)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Raqib (الرقيب)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mujib (المجيب)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wasi (الواسع)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hakim (الحكيم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wadud (الودود)
+          </Text>
+          <Text style={styles.text}>
+            Al-Majid (المجيد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Ba'ith (الباعث)
+          </Text>
+          <Text style={styles.text}>
+            Ash-Shahid (الشهيد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Haqq (الحق)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wakil (الوكيل)
+          </Text>
+          <Text style={styles.text}>
+            Al-Qawiyy (القوى)
+          </Text>
+          <Text style={styles.text}>
+            Al-Matin (المتين)
+          </Text>
+          <Text style={styles.text}>
+            Al-Waliyy (الولى)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hamid (الحميد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muhsi (المحصى)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mubdi' (المبدئ)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mu'id (المعيد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muhyi (المحيى)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mumit (المميت)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hayy (الحي)
+          </Text>
+          <Text style={styles.text}>
+            Al-Qayyum (القيوم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wajid (الواجد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Majid (الماجد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wahid (الواحد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Ahad (الاحد)
+          </Text>
+          <Text style={styles.text}>
+            As-Samad (الصمد)
+          </Text>
+          <Text style={styles.text}>
+            Al-Qadir (القادر)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muqtadir (المقتدر)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muqaddim (المقدم)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mu'akhkhir (المؤخر)
+          </Text>
+          <Text style={styles.text}>
+            Al-Awwal (الأول)
+          </Text>
+          <Text style={styles.text}>
+            Al-Akhir (الأخر)
+          </Text>
+          <Text style={styles.text}>
+            Az-Dhahir (الظاهر)
+          </Text>
+          <Text style={styles.text}>
+            Al-Batin (الباطن)
+          </Text>
+          <Text style={styles.text}>
+            Al-Wali (الوالي)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muta'ali (المتعالي)
+          </Text>
+          <Text style={styles.text}>
+            Al-Barr (البر)
+          </Text>
+          <Text style={styles.text}>
+            At-Tawwab (التواب)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muntaqim (المنتقم)
+          </Text>
+          <Text style={styles.text}>
+            Al-'Afuww (العفو)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Ra'uf (الرؤوف)
+          </Text>
+          <Text style={styles.text}>
+            Malik-al-Mulk (مالك الملك)
+          </Text>
+          <Text style={styles.text}>
+            Dhu-al-Jalal wa-al-Ikram (ذو الجلال و الإكرام)
+          </Text>
+          <Text style={styles.text}>
+            Al-Muqsit (المقسط)
+          </Text>
+          <Text style={styles.text}>
+            Al-Jami' (الجامع)
+          </Text>
+          <Text style={styles.text}>
+            Al-Jami' (الجامع)
+          </Text>
+          <Text style={styles.text}>
+            Al-Ghani (الغنى)
+          </Text>
+          <Text style={styles.text}>
+            Al-Mughni (المغنى)
+          </Text>
+          <Text style={styles.text}>
+            Ad-Darr (الضار)
+          </Text>
+          <Text style={styles.text}>
+            An-Nafi' (النافع)
+          </Text>
+          <Text style={styles.text}>
+            An-Nur (النور)
+          </Text>
+          <Text style={styles.text}>
+            Al-Hadi (الهادي)
+          </Text>
+          <Text style={styles.text}>
+            Al-Badi (البديع)
+          </Text>
+          <Text style={styles.text}>
+            Al-Baqi (الباقي)
+          </Text>
+          <Text style={styles.text}>
+            Al-Warith (الوارث)
+          </Text>
+          <Text style={styles.text}>
+            Ar-Rashid (الرشيد)
+          </Text>
+          <Text style={styles.text}>
+            As-Sabur (الصبور)
+          </Text>
+        </ScrollView>
+      </View>
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  headerContainer: {
+    top: 200
+  },
+  namesContainer: {
+    top: 50,
+    height: 170,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    borderWidth: 4,
+    borderRadius: 0,
+    padding: 10,
+    color: '#ffffff',
+    backgroundColor: '#ff8080',
+    borderColor: '#ffffff'
+  },
+
+  headerText: {
+    color: '#1aedbb',
+    fontWeight: 'bold',
+    fontSize: 50
+  }
+});
